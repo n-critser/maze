b5e2ac5e0e0c0b0d71d1ce80784713120154463a
n-critser <nick@wordtronix.org>
2016-02-07 17:02:22 -0500

    Add win test & directions for maze movement
    Added enum for directions
    Add movement in maze Add win testing

--- interface/mazeFace.c
+++ interface/mazeFace.c
@@ -16,6 +16,10 @@
 };
  */
 
+enum DIRECTION {north,east,south,west};
+
+
+
 struct maze_face{
         char **board;
         int width;
@@ -23,8 +27,11 @@ struct maze_face{
         int maze_num;
         int score;
         int x,y;
+        int win;
+        enum DIRECTION moveDir;
         struct vertex *goal;
         struct vertex *player;
+        char pChar,gChar;
         
 };
 
@@ -56,6 +63,9 @@ int set_maze_ioconf(){
 
 int maze_game_init(struct maze_face *face, int width,int height){
         int x,y;
+        face->win=0;
+        face->pChar='@';
+        face->gChar='#';
         face->player=malloc(sizeof(struct vertex));
         face->goal=malloc(sizeof(struct vertex));
         face->width=width;
@@ -75,6 +85,46 @@ int maze_game_init(struct maze_face *face, int width,int height){
         return 0;
 }
 
+int maze_hit_test(struct maze_face *face){
+
+        //int x,y;
+        
+        if ((face->player-> y == face->goal-> y) && (face->player->x == face->goal->x)){
+                face->win=1;
+        } else if  ((face->player->y > face->height ) ||
+                    (face->player->y < 0)             ||
+                    (face->player->x < 0)             ||
+                    (face->player->x > face->width)){
+                face->win=-1;
+        } else
+                face->win=0;
+        return face->win;
+}
+
+
+int maze_move(struct maze_face *face, enum DIRECTION move){
+        printf ("move : %d \n",move);
+        switch(move){
+        case north:  
+                face->player->y--;
+                break;
+
+        case east:
+                face->player->x++;
+                break;
+        case south:
+                face->player->y++;
+                break;
+        case west:
+                face->player->x--;
+                break;
+        }
+        // test for hit after move
+        // could stop move pre-emptively using hit test
+        maze_hit_test(face);
+        return move;
+}
+
 int maze_face_clean(struct maze_face *face) {
 	int x;
 	for (x=0; x<face->width; x++) {
@@ -84,11 +134,14 @@ int maze_face_clean(struct maze_face *face) {
         return 0;
 }
 
+
 void maze_show_face(struct maze_face * face){
         int x,y;
         int space=20;
         for(x=0; x<space;x++)
                 printf("\n");
+        printf ("moveDir = %d\n",face->moveDir);
+        printf ("face->win = %d\n",face->win);
         printf("[MAZE#: %d | SCORE: %d]\n", face->maze_num, face->score);
         printf("player(x,y):(%d,%d) ",face->player->x,face->player->y);
         printf("goal(x,y):(%d,%d) ",face->goal->x,face->goal->y);
@@ -100,7 +153,14 @@ void maze_show_face(struct maze_face * face){
         for (y=0; y<face->height;y++){
                 printf("|");
                 for (x=0; x<face->width;x++){
-                        printf("%c ",face->board[x][y]);
+                        if ( (y == face->player->y) && (x == face->player-> x)){
+                                printf("%c ",face->pChar);
+                               
+                        } else if ((y == face->goal->y) && (x == face->goal-> x)){
+                                printf("%c ",face->gChar);
+                        } else {
+                                printf("%c ",face->board[x][y]);
+                        }
                 }
                 printf("|\n");
         }
@@ -130,20 +190,39 @@ int maze_quit(struct maze_face * face){
 int maze_play(int width, int height){
         struct maze_face face;
         char cmd;
-        
+        enum DIRECTION move;
         set_maze_ioconf();
         maze_game_init(&face,width,height);
         maze_show_face(&face);
-        while((cmd=getchar())>0){
-                switch(cmd){
-                case 't':
-                        face.y--;
-                        break;
-                default:
-                        face.x=10;
-                        face.y=10;
-                        break;
+        while( face.win >= 0){
+                while((cmd=getchar())>0){
+                        switch(cmd){
+                        case 'e':
+                                move=north;
+                                maze_move(&face,move);
+                                break;
+                        case 'f':
+                                move=east;
+                                maze_move(&face,move);
+                                break;
+                        case 'd':
+                                move=west;
+                                maze_move(&face,move);
+                                break;
+                        case 'v':
+                                move=south;
+                                maze_move(&face,move);
+                                break;
+                        default:
+                                face.x=10;
+                                face.y=10;
+                                break;
+                        }
+                        maze_test_win(&face);
+                        maze_show_face(&face);
+                        
                 }
+                
         }
         maze_quit(&face);
 
