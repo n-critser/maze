3a5df011f8e1374ed44bc0904fffb6fa93c23121
n-critser <nick@wordtronix.org>
2016-03-20 22:18:59 -0400

    add print based on  Douglas Wilhelm Harder maze generator
    Add maze print routine to support spanning tree

--- graph/prototype/mazeBuilder.py
+++ graph/prototype/mazeBuilder.py
@@ -27,6 +27,7 @@ moves=("north","east","south","west")
 print moves
 print moves[1]
 def maze_init_legal_move_list(graph,source):
+    """create list of legal moves based on graph dims"""
     legalMoves=[]
     if (source % graph.dim) == 0:
         assert source % graph.dim ==0
@@ -83,31 +84,117 @@ def maze_init_legal_move_list(graph,source):
     # else out of bounds ?
     print legalMoves
     return legalMoves
+
 def maze_check_legal_move(graph,source,sink):
-    moves=maze_init_legal_move_list(graph,source)
-    if sink in moves:
+    moveList=maze_init_legal_move_list(graph,source)
+    if sink in moveList:
         print "move **IS** legal"
         return True
     else:
         print "move is *NOT* legal"
         return False
+
+    
 def maze_add_edge(graph,source,sink):
+    """adds edge from source to sink
+    after testing if move is legal"""
     if (maze_check_legal_move(graph,source,sink)):
         # directed graph add only single edge
         graph.graph_add_edge(source,sink)
         # undirected graph add reverse edge 
         graph.graph_add_edge(sink,source)
+        return True
     print "source: %d , sink: %d"%(source,sink)
     print graph.graph_return_stats()
-    return 0
+    return False
+
 def maze_init(vertices,dimension):
     """initialize a maze  """
     mGraph=gh.Graph(vertices,dimension)
-    # create legal move list
-    # create
-    print mGraph.graph_return_stats()    
     return mGraph
 
+
+def maze_build_path(graph,source,sink,searchMethod):
+    # use dfs or bfs to build a path from source to sink
+    path = []
+    
+    if maze_add_edge(graph,source,sink):
+        return 0
+    else :
+        moveList=maze_init_legal_move_list(graph,source)
+        while(source != sink):
+            maze_add_edge(graph,source,moveList[1])
+        
+    return 1
+
+#int number_of_walls = 2*m*n - m - n;
+#		bool is_wall[number_of_walls];
+
+
+# format and print behavior from 
+# Douglas Wilhelm Harder
+# https://ece.uwaterloo.ca/~dwharder/aads/Algorithms/Maze_generation/
+def maze_print(graph):
+    n = graph.n/graph.dim
+    m = graph.dim
+    print "n: %d , m: %d"%(n,m)
+    is_wall=[]
+    for r in range(0,(2*m*n-m-n)):
+        is_wall.append(True)
+    topString=""
+
+    print "+  +",
+    
+    for i in range(0, n - 1 ):
+        print "--+",
+        wall_counter = 0
+    print ""
+    for i in range(0, m - 1):
+        # Print the vertical walls of the ith row.
+        print "|",
+        for j in range(0,n-1):
+            if ( is_wall[wall_counter] ) :
+                    print "  |",
+            else :
+                print "   ",
+            ++wall_counter
+        # end inner vertical wall 
+        print "  |" 
+        
+        # Print the horizontal walls of the ith row:
+        #  - these are the walls between the ith and (i + 1)st rows.
+        print "+",
+        for j in range(0, n ):
+            if ( is_wall[wall_counter] ) :
+                print "--+",
+            else :
+                print "  +",
+		
+
+            ++wall_counter
+        print ""
+        # end horizontal walls of ith row
+    # end ith row printing 
+    #Print the horizontal(vertical?) walls of the last row.
+    print "|",
+    for k in range(0,n-1):
+        if ( is_wall[wall_counter] ):
+            print "  |",
+        else :
+            print "   ",
+            
+                
+        ++wall_counter
+    print "  |" 
+    # end
+    #Print the bottom border.
+    for p in range(0,n-1):
+        print "+--",
+    print "+  +\n" 
+    # end
+
+#################################################
+## testing 
 graph = maze_init(32,8)
 
 maze_add_edge(graph,1,2)
@@ -120,4 +207,5 @@ maze_add_edge(graph,10,18)
 maze_add_edge(graph,30,31)
 maze_add_edge(graph,26,18)
 print graph
-print graph.graph_show_graph()
+#print graph.graph_show_graph()
+maze_print(graph)
