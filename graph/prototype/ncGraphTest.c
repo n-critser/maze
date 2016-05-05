502cea3a68b08ab85a7ebcf572bc5ca30fa65dec
n-critser <nick@wordtronix.org>
2016-05-05 08:21:01 -0400

    Add tests for outdegree of vertex + edgecount

--- graph/prototype/ncGraphTest.c
+++ graph/prototype/ncGraphTest.c
@@ -12,11 +12,20 @@ int main(int argc, char ** argv){
         Graph gh;
         int rows;
         int cols;
-
+        
+        
         gh = graph_create(TEST1_SIZE);
         assert(graph_vertex_count(gh) == TEST1_SIZE);
-        graph_add_edge(gh,TEST1_SIZE-1,TEST1_SIZE-2);
+
+        /* */
+        graph_add_edge(gh,1,2);
         assert(graph_edge_count(gh)==1);
+        assert(graph_has_edge(gh,1,2));
+        assert(graph_out_degree(gh,1)==1);
+        /*
+          graphs are directed so below fails
+          assert(graph_has_edge(gh,2,1));       
+        */
         
         return 0;
 }
